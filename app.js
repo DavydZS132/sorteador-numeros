c24253077a18f6ddf042afcb9c8a3f430c157831
Davyd Aparecido Zuquim Silva <dzuquim10@gmail.com>
2025-12-05 11:55:15 -0300

    Colocando as Variavel

---
+++ app.js
@@ -0,0 +1,7 @@
+function sortear() {
+    let quantidade = document.getElementById('quantidade').value;
+    let de = document.getElementById('de').value;
+    let ate = document.getElementById('ate').value;
+
+    console.log(`Quantidade: ${quantidade}, Do número: ${de}, Até o número: ${ate}`);
+}
